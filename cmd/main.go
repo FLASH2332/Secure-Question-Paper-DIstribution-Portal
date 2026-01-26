ca17f094fed77898823aabc8bb100628df0954da
FLASH2332 <jayaveeran2005@gmail.com>
2026-01-26 17:47:05 +0530

    feat : Add sqlite connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+)
 
-func main(){
-	fmt.Println("Hello world")
+func main() {
+    fmt.Println("🔐 Secure Exam System - Setup Verified!")
+    
+    // Test SQLite connection
+    db, err := sql.Open("sqlite3", "./storage/exam.db")
+    if err != nil {
+        fmt.Println("Database connection failed:", err)
+        return
+    }
+    defer db.Close()
+    
+    fmt.Println("Database connection successful!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FLASH2332/Secure-Question-Paper-Distribution-Portal
 
 go 1.24.2
+
+require github.com/mattn/go-sqlite3 v1.14.33
